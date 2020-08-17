2d7f0ef6d172e8d158668b3f94b17d79d8944ee0
Andrew3203 <kuand3203@gmail.com>
2020-08-17 14:51:04 +0300

    add mirea

--- parsers.py
+++ parsers.py
@@ -4,7 +4,6 @@ import requests as req
 
 def mirea_parser(fio, link):
     try:
-        name1, last_name1, middle_name1, ege = fio.split('. ')
         resp = req.get(link)
         soup = BeautifulSoup(resp.text, 'html.parser')
         for obj in soup.find_all('tr'):
@@ -12,7 +11,9 @@ def mirea_parser(fio, link):
             if name is not None:
                 ans = name.text.strip().split(' ')
                 if len(ans) == 3 and \
-                        ans[0] == last_name1 and ans[1] == name1 and ans[2] == middle_name1:
+                        ans[0] == fio['last__name'] and \
+                        ans[1] == fio['first_name'] and \
+                        ans[2] == fio['middle_name']:
                     accepted = obj.find("td", {"class": "accepted"})
                     pos = obj.find("td", {"class": "num"})
                     score = obj.find("td", {"class": "sum"})
