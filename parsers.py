796924eeb50e394153bda1c09bd200d0c286856c
Andrew3203 <kuand3203@gmail.com>
2020-08-17 14:38:38 +0300

    add mirea

--- parsers.py
+++ parsers.py
@@ -3,21 +3,22 @@ import requests as req
 
 
 def mirea_parser(fio, link):
-    name1, last_name1, middle_name1, ege = fio.split('. ')
-    resp = req.get(link)
-    soup = BeautifulSoup(resp.text, 'html.parser')
-    for obj in soup.find_all('tr'):
-        name = obj.find("td", {"class": "fio"})
-        if name is not None:
-            ans = name.text.strip().split(' ')
-            print(ans)
-            if len(ans) == 3 and \
-                    ans[0] == last_name1 and ans[1] == name1 and ans[2] == middle_name1:
-                accepted = obj.find("td", {"class": "accepted"})
-                pos = obj.find("td", {"class": "num"})
-                score = obj.find("td", {"class": "sum"})
-                return [pos.text, accepted.text, score.text]
-
+    try:
+        name1, last_name1, middle_name1, ege = fio.split('. ')
+        resp = req.get(link)
+        soup = BeautifulSoup(resp.text, 'html.parser')
+        for obj in soup.find_all('tr'):
+            name = obj.find("td", {"class": "fio"})
+            if name is not None:
+                ans = name.text.strip().split(' ')
+                if len(ans) == 3 and \
+                        ans[0] == last_name1 and ans[1] == name1 and ans[2] == middle_name1:
+                    accepted = obj.find("td", {"class": "accepted"})
+                    pos = obj.find("td", {"class": "num"})
+                    score = obj.find("td", {"class": "sum"})
+                    return [pos.text, accepted.text, score.text]
+    except Exception as e:
+        print(e)
     return ['ошибка', 'ошибка', 'ошибка']
 
 
